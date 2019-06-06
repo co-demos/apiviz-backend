a0c70edd8380adcf88daae2ff30d299808587a34
Julien_P <jparis.py@gmail.com>
2019-06-06 16:57:29 +0200

    fix on nwsgi_prod.py

--- wsgi_prod.py
+++ wsgi_prod.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 ### + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ###
 from dotenv import load_dotenv
 from pathlib import Path  # python3 only
-env_path_global = Path('.') / 'example.env.global'
+env_path_global = Path('.') / '.env.global'
 env_path_mongodb = Path('.') / '.env.mongodb'
 load_dotenv(env_path_global, verbose=True)
 load_dotenv(env_path_mongodb, verbose=True)
