56700d7a210ad78bd789d64b58692e9041da9a1d
Julien_P <jparis.py@gmail.com>
2019-11-11 13:23:50 +0100

    cleaning imports form config folders at __init__ and /boot + added some new default uuids

--- backend/config_app/__init__.py
+++ backend/config_app/__init__.py
@@ -12,9 +12,12 @@ uuid_models = {
   "uuid_apcis" :          "f0a482da-28be-4929-a443-f22ecb03ee68",
   "uuid_sonum" :          "c5efafab-1733-4ad1-9eb8-d529bc87c481",
   "uuid_tiers_lieux" :    "fd9d4302-bddb-4fb1-8f13-d64dfdb66b91",
+
   "uuid_ping_carto" :     "0278419c-558e-43d5-a4d6-c836afd10445",
   "uuid_ocf" :            "305ab50d-c976-44d7-a8f2-a7594155c292",
   "uuid_open_archives" :  "0278419c-558e-43d5-a4d6-c836afd10445",
+
+  "uuid_apiviz_demo" :    "b565372d-c33f-450b-9aef-5aca3b5c628e",
 }
 
 config_folders = [
