a0c4618d78fbc8d2448aa51f0370b83ab47b1857
Julien_P <jparis.py@gmail.com>
2019-11-27 19:33:17 +0100

    gallery dropdown in navbar apiviz demo site

--- backend/config_app/config_demo_apiviz/config_app_navbar.py
+++ backend/config_app/config_demo_apiviz/config_app_navbar.py
@@ -64,6 +64,7 @@ default_app_navbar = [
                 # { "is_divider" : False, "is_external_link" : True, "link_to" : "https://forum.societenumerique.gouv.fr/category/10/cartographie-des-services-de-m%C3%A9diation-et-d-inclusion-num%C3%A9rique", "link_text" : [{"locale" : "en", "text" : "Contribute to the map"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Participez à la cartographie"}] },
               ]
             },
+
             { "is_visible" : True,
               "position"   : "exterior_right",
               "link_to"    : "/tiers-lieux",
@@ -81,6 +82,26 @@ default_app_navbar = [
               ]
             },
 
+            { "is_visible" : True,
+              "position"   : "exterior_right",
+              "link_to"    : "/galery",
+              "help"       : u"First menu in navbar",
+              "is_external_link" : False,
+              "link_type"  : "link", ### show btn border
+              "icon_class" : "",
+              "link_text"  : [{"locale" : "en", "text" : "Gallery"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Galerie" }],
+              "tooltip"    : [{"locale" : "en", "text" : "More infos on the topic"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Plus d’informations sur ce site" }],
+              "has_dropdown" : True,
+              "dropdowns"  : [
+                # { "is_divider" : False, "is_external_link" : False, "link_to" : "/galery/summary",               "link_text" : [{"locale" : "en", "text" : "Summary"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Sommaire"}] },
+                # { "is_divider" : True,  "is_external_link" : False },
+                { "is_divider" : False, "is_external_link" : True, "link_to" : "https://orgues-apiviz.netlify.com/",         "link_text" : [{"locale" : "en", "text" : "Organons"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Inventaire des orgues"}] },
+                { "is_divider" : False, "is_external_link" : True, "link_to" : "https://tiers-lieux-dataviz.netlify.com/",   "link_text" : [{"locale" : "en", "text" : "Third places"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Tiers lieux"}] },
+                { "is_divider" : False, "is_external_link" : True, "link_to" : "https://apiviz-preprod-sonum.netlify.com/",  "link_text" : [{"locale" : "en", "text" : "Sonum"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Carto Sonum"}] },
+                { "is_divider" : False, "is_external_link" : True, "link_to" : "https://etalab-codes.netlify.com/a-propos",  "link_text" : [{"locale" : "en", "text" : "Etalab codes"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Codes Etalab"}] },
+                { "is_divider" : False, "is_external_link" : True, "link_to" : "https://ping-carto.netlify.com/",            "link_text" : [{"locale" : "en", "text" : "PiNG carto"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Carto Ping"}] },
+              ]
+            },
 
           ]
         },
