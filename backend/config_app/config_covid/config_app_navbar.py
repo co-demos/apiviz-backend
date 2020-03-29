79e6625c3d7622d59927c89ee61df8f3f382d943
Julien_P <jparis.py@gmail.com>
2020-03-29 19:42:30 +0200

    more on menus covid

--- backend/config_app/config_covid/config_app_navbar.py
+++ backend/config_app/config_covid/config_app_navbar.py
@@ -64,8 +64,8 @@ default_app_navbar = [
               "has_dropdown" : True,
               "dropdowns"  : [
                 { "is_divider" : False, "is_external_link" : False, "link_to" : "/printers", "link_text" : [{"locale" : "en", "text" : "3D printers map"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "Carte des imprimeurs 3D"}] },
-                { "is_divider" : True,  "is_external_link" : False },
-                { "is_divider" : False, "is_external_link" : False, "link_to" : "/map-printers", "link_text" : [{"locale" : "en", "text" : "-->   Handdle Map 3D printers"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "-->   Carte Handdle impression 3D"}] },
+                # { "is_divider" : True,  "is_external_link" : False },
+                # { "is_divider" : False, "is_external_link" : False, "link_to" : "/map-printers", "link_text" : [{"locale" : "en", "text" : "-->   Handdle Map 3D printers"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "-->   Carte Handdle impression 3D"}] },
               ]
             },
 
