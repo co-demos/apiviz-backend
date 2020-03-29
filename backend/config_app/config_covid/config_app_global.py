38fffa72567bffaea6e4ca8bd7d30c905ef06e16
Julien_P <jparis.py@gmail.com>
2020-03-29 18:37:31 +0200

    more on menus covid

--- backend/config_app/config_covid/config_app_global.py
+++ backend/config_app/config_covid/config_app_global.py
@@ -176,7 +176,7 @@ default_global_config = [
         "image_preview" : "https://raw.githubusercontent.com/co-demos/PING-carto/master/documentation/screenshots/map-view-tiers-lieux-01.png",
 
         "content"      : u"Covid-initiatives",
-        "content_text" : [{"locale" : "en", "text" : "Initiatives against covid-19"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "covid-initiatives.org"}],
+        "content_text" : [{"locale" : "en", "text" : "COVID-INITIATIVES"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "COVID-INITIATIVES"}],
         "is_in_navbar" : True,
         # "title_color" : "primary",
 
