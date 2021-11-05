a1fb3a017db4f039fb4f764d3a5a57943a964a78
Julien_P <jparis.py@gmail.com>
2021-11-05 16:25:52 +0100

    more odf config

--- backend/config_app/config_open_data_france/config_app_global.py
+++ backend/config_app/config_open_data_france/config_app_global.py
@@ -93,7 +93,7 @@ default_global_config = [
         "image_preview" : "https://raw.githubusercontent.com/co-demos/ODF-carto/master/documentation/screenshots/map-view-tiers-lieux-01.png",
 
         "content"      : u"Carto Open data France",
-        "content_text" : [{"locale" : "en", "text" : "A lookout at third places in Loire region"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "carto Observatoire open data"}],
+        "content_text" : [{"locale" : "en", "text" : "A lookout at third places in Loire region"},{"locale" : "es", "text" : "pendiente"},{"locale" : "tr", "text" : "yapılmamış"},{"locale" : "de", "text" : "ungemacht"}, {"locale" : "fr", "text" : "carte de l'observatoire open data"}],
         "content_size" : 7,
         "is_in_navbar" : True,
         # "title_color" : "primary",
