cfb91d1875be11a85f99b9433e453e33495e101d
Julien_P <jparis.py@gmail.com>
2021-12-14 12:04:13 +0100

    update odf gradient choropleth

--- backend/config_app/config_open_data_france/config_app_data_endpoints.py
+++ backend/config_app/config_open_data_france/config_app_data_endpoints.py
@@ -610,7 +610,7 @@ default_data_endpoints_config = [
           {  "app_arg" : "forMap",       "arg" : "map_list",          "optional" : False, "in" : ["url"], "default" : True,        "type": "bool" },
           # {  "app_arg" : "defaultValue", "arg" : "fields_to_return",  "optional" : False, "in" : ["url"], "default" : "INSEEDEP",  "type": "str" },
           # {  "app_arg" : "defaultValue", "arg" : "fields_to_return",  "optional" : False, "in" : ["url"], "default" : "result_context,DEPARTEMENT,INDEX,NOM DU LIEU",  "type": "str" },
-          {  "app_arg" : "defaultValue", "arg" : "fields_to_return",  "optional" : False, "in" : ["url"], "default" : "nom,id_odf,type,platform_clean,depcode,tags,type",  "type": "str" },
+          {  "app_arg" : "defaultValue", "arg" : "fields_to_return",  "optional" : False, "in" : ["url"], "default" : "nom,id_odf,type,platform_clean,depcode,tags",  "type": "str" },
           # {  "app_arg" : "asLatLng", "arg" : "as_latlng",         "optional" : False, "in" : ["url"], "default" : True, "type": "bool" },
           # {  "app_arg" : "onlyGeocoded", "arg" : "only_geocoded", "optional" : False, "in" : ["url"], "default" : True, "type": "bool" },
 
