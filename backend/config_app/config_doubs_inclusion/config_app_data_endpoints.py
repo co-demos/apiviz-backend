48550e83b65fad5b6e8b5b34e1865d9b04dbd51f
Julien_P <jparis.py@gmail.com>
2022-03-24 12:06:33 +0100

    update doubs config

--- backend/config_app/config_doubs_inclusion/config_app_data_endpoints.py
+++ backend/config_app/config_doubs_inclusion/config_app_data_endpoints.py
@@ -519,7 +519,7 @@ default_data_endpoints_config = [
 
           {  "app_arg" : "forMap",       "arg" : "map_list",          "optional" : False, "in" : ["url"], "default" : True,        "type": "bool" },
           # {  "app_arg" : "defaultValue", "arg" : "fields_to_return",  "optional" : False, "in" : ["url"], "default" : "INSEEDEP",  "type": "str" },
-          {  "app_arg" : "defaultValue", "arg" : "fields_to_return",  "optional" : False, "in" : ["url"], "default" : "COM_CODE,carto_id,NOM_DISPOSITIF,TAGS,TYPE_STRUCTURE",  "type": "str" },
+          {  "app_arg" : "defaultValue", "arg" : "fields_to_return",  "optional" : False, "in" : ["url"], "default" : "COM_CODE,carto_id,NOM_DISPOSITIF,TAGS,TYPE_STRUCTURE,ADRESSE_COMPLETE",  "type": "str" },
           # {  "app_arg" : "asLatLng", "arg" : "as_latlng",         "optional" : False, "in" : ["url"], "default" : True, "type": "bool" },
           # {  "app_arg" : "onlyGeocoded", "arg" : "only_geocoded", "optional" : False, "in" : ["url"], "default" : True, "type": "bool" },
 
